f653c062e5bc8a7bf78ed4dda0df3a81f709f176
Crozy <pololeuck@gmail.com>
2019-02-19 11:49:27 +0100

    aspi tourne toujours à gauche

--- src/application/Algo.java
+++ src/application/Algo.java
@@ -9,6 +9,7 @@ public class Algo {
 	private Tool outils;
 	private int tour;
 	private Position positionAspi = new Position(0, 0);
+	private String actionPrec = "recule";
 
 	public Algo(AnchorPane panel, Box aspi) {
 		outils = new Tool(panel.getChildren().listIterator());
@@ -16,21 +17,49 @@ public class Algo {
 
 	public void Parti(Box aspi) {
 		System.out.println("Tour : " + tour);
-//		if (positionAspi.x == aspi.getLayoutX() && positionAspi.y == aspi.getLayoutY()) {
 		tour++;
-//			if (tour > 50) {
-//				outils.haut(aspi);
-//				positionAspi.x = aspi.getLayoutX();
-//				positionAspi.y = aspi.getLayoutY();
-//			}
-//		} else {
-//			outils.avance(aspi);
-//			positionAspi.x = aspi.getLayoutX();
-//			positionAspi.y = aspi.getLayoutY();
-//		}
+
+		if (outils.recherche(aspi, actionPrec)) {
+			switch (actionPrec) {
+			case "avance":
+				outils.avance(aspi);
+				break;
+			case "recule":
+				outils.recule(aspi);
+				break;
+			case "haut":
+				outils.haut(aspi);
+				break;
+			case "bas":
+				outils.bas(aspi);
+				break;
+			default:
+				break;
+			}
+		} else {
+
+			if (outils.recherche(aspi, "avance")) {
+				outils.avance(aspi);
+				actionPrec = "avance";
+			} else {
+				if (outils.recherche(aspi, "haut")) {
+					outils.haut(aspi);
+					actionPrec = "haut";
+				} else {
+					if (outils.recherche(aspi, "recule")) {
+						outils.recule(aspi);
+						actionPrec = "recule";
+					} else {
+						if (outils.recherche(aspi, "bas")) {
+							outils.bas(aspi);
+							actionPrec = "bas";
+						}
+					}
+				}
+			}
+		}
 
 		System.out.println("Radar : " + outils.recherche(aspi, "avance"));
-		outils.recule(aspi);
 	}
 
 }
